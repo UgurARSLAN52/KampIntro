fb52d6055d7880af03e7ed59c10cf31def89cf3b
ugur5 <ugur5@UGURARSLAN>
2024-12-07 18:34:59 +0300

    #

--- DegerVeReferansTipler/Program.cs
+++ DegerVeReferansTipler/Program.cs
@@ -10,4 +10,7 @@ int[] sayilar1 = new int[] {10, 20 ,30};
 int[] sayilar2 = new int[] {100, 200 ,300};
 sayilar1 = sayilar2;
 sayilar2[0] = 999;
-// sayilar1[0] ?
+// sayilar1[0] ?     
+
+
+int denemeSayi = 111;
